f1e3f7b479c41bd92d9e91d9cf8f877dd9620e36
rukshan <thilakarathnarukshan9@gmail.com>
2024-06-17 00:17:08 +0530

    rukshan-thilakarathna-git-up-Mon Jun 17 12:17:08 AM +0530 2024

--- public/admin/ADM_FOOT.php
+++ public/admin/ADM_FOOT.php
@@ -11,13 +11,84 @@ _('ys3').onclick=function(){scrollToTop(document.documentElement.scrollHeight);}
         function myCategoryFunction() {
             var x = document.getElementById("zyca").value;
 
+            const xhttp = new XMLHttpRequest();
+            xhttp.onload = function() {
+                // var text = ""; // Initialize the text variable
+                var response = this.responseText; // Get the response text
+                var lis = document.querySelectorAll('#subzyca');
+
+                lis.forEach(function(element) {
+                    // Remove all child elements of the current element
+                    while (element.firstChild) {
+                        element.removeChild(element.firstChild);
+                    }
+                });
+
+                const jsonObject = JSON.parse(response)
+                var dataLength = jsonObject.length;
+                // console.log(jsonObject)
+                // console.log(response)
+                console.log(dataLength)
+
+                if (dataLength > 0){
+                    document.getElementById('subcatfild').style.display='block';
+
+                    document.getElementById('subsubcatfild').style.display='none';
+                    for(let i=0; i<dataLength; i++){
+                        const option = document.createElement('option');
+                        option.value = jsonObject[i]['cid'];
+                        option.textContent = jsonObject[i]['cn'];
+                        document.getElementById("subzyca").appendChild(option);
+                    }
+                }else {
+                    document.getElementById('subcatfild').style.display='none';
+                    document.getElementById('subsubcatfild').style.display='none';
+                }
+            }
+            xhttp.open("GET", "admin/ajaxAddItemCategory.php?mainCategoryId=" + x, true);
+            xhttp.send();
+        }
+
+        function mySubCategoryFunction() {
+            var x = document.getElementById("subzyca").value;
 
             const xhttp = new XMLHttpRequest();
             xhttp.onload = function() {
-                console.log(this.responseText)
+                // var text = ""; // Initialize the text variable
+                var response = this.responseText; // Get the response text
+                var lis = document.querySelectorAll('#subsubzyca');
+
+                lis.forEach(function(element) {
+                    // Remove all child elements of the current element
+                    while (element.firstChild) {
+                        element.removeChild(element.firstChild);
+                    }
+                });
+
+                const jsonObject = JSON.parse(response)
+                var dataLength = jsonObject.length;
+                // console.log(jsonObject)
+                // console.log(response)
+                console.log(dataLength)
+
+                if (dataLength > 0){
+                    document.getElementById('subsubcatfild').style.display='block';
+                    for(let i=0; i<dataLength; i++){
+                        const option = document.createElement('option');
+                        option.value = jsonObject[i]['cid'];
+                        option.textContent = jsonObject[i]['cn'];
+                        document.getElementById("subsubzyca").appendChild(option);
+                    }
+                }else {
+                    document.getElementById('subsubcatfild').style.display='none';
+                }
+
+                // console.log(response)
             }
-            xhttp.open("GET", "admin/ajaxAddItemCategory.php?mainCategoryId="+x, true);
+            xhttp.open("GET", "admin/ajaxAddItemSubCategory.php?mainCategoryId=" + x, true);
             xhttp.send();
+
+
         }
     </script>
 </footer>
--- public/admin/ajaxAddItemSubCategory.php
+++ public/admin/ajaxAddItemSubCategory.php
@@ -5,7 +5,7 @@ $MainCategoryId = $_GET['mainCategoryId'] ?? false ;
 if ($MainCategoryId){
 
 
-    $sc = $db->prepare("SELECT `cid`, `cn` FROM `categories` WHERE `scid`=? AND `az`=?");
+    $sc = $db->prepare("SELECT `cid`, `cn` FROM `categories` WHERE `ssid`=? AND `az`=?");
     $sc->execute([$MainCategoryId,1]);
     $cat = $sc->fetchAll();
 //    $arr_cat = array();
